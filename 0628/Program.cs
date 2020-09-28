a6961648844a01022663e1e5578d6259b29ea2ff
Szilágyi Gergely Csaba <szilagyi.gergely@vereb.dc>
2020-09-28 08:44:11 +0200

    Add project files.

---
+++ 0628/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0628
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I have " + rnd.Next(1, 21) + " aple pie");
+            Console.ReadKey();
+        }
+    }
+}
